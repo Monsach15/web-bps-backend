47f015640dec30ecabb7e95b8d16979b631cefb3
Amanda <claymrx@gmail.com>
2025-07-13 17:18:43 +0700

    edit api

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,25 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\PublikasiController;
 
+
+Route::post('/login', [AuthController::class, 'login']); 
+
+Route::middleware('auth:sanctum')->group(function () {
+    
+    Route::get('/user', function (Request $request){
+        return $request->user();
+    });
+    Route::post('/logout', [AuthController::class, 'logout']);
+
+    // Publikasi
+    Route::get('/publikasi', [PublikasiController::class, 'index']);
+    Route::post('/publikasi', [PublikasiController::class, 'store']);
+    Route::get('/publikasi/{id}', [PublikasiController::class, 'show']); 
+    Route::put('/publikasi/{id}', [PublikasiController::class, 'update']); 
+    Route::delete('/publikasi/{id}', [PublikasiController::class, 'destroy']); 
+});
+
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -15,25 +34,3 @@ use App\Http\Controllers\PublikasiController;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
-
-Route::post('/login', [AuthController::class, 'login']);
-
-Route::middleware('auth:sanctum')->group(function () {
-    // Publikasi
-    Route::get('/publikasi', [PublikasiController::class, 'index']);
-    Route::post('/publikasi', [PublikasiController::class, 'store']);
-    Route::get('/publikasi/{id}', [PublikasiController::class, 'show']);
-    Route::put('/publikasi/{id}', [PublikasiController::class, 'update']);
-    Route::delete('/publikasi/{id}', [PublikasiController::class, 'destroy']);
-
-    //Logout
-    Route::post('/logout', [AuthController::class, 'logout']);
-});
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
-Route::get('/tes', function () {
-    return 'tes berhasil';
-});
